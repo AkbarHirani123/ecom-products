39384ca6b4e8aa147e164984cc999195365b9b33
Akbar Hirani <akbar.hirani123@gmail.com>
2020-08-26 22:26:19 -0400

    try heroku deploy again

--- dist/dist/server.dev.js
+++ dist/dist/server.dev.js
@@ -2,8 +2,6 @@
 
 var _express = _interopRequireDefault(require("express"));
 
-var _data = _interopRequireDefault(require("./data"));
-
 var _config = _interopRequireDefault(require("./config"));
 
 var _mongoose = _interopRequireDefault(require("mongoose"));
